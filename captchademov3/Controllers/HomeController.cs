7776762ab78b91cb30736a5cfb339edeb28df828
AnSharma <AnSharma@LAP-55379>
2021-12-14 10:59:00 +0530

    Add project files.

---
+++ captchademov3/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using captchademov3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace captchademov3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Student student = new Student();
+            return View(student);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Result(Student s)
+        {
+            //var _googlerecaptcha=GoogleCaptchaService.verifyrecaptcha()
+            GoogleCaptchaService googleCaptchaService = new GoogleCaptchaService();
+            var _googlecaptcha = await googleCaptchaService.verifycaptcha(s.token);
+            if(_googlecaptcha)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+            return View("Result", s);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ captchademov3/Models/GoogleCaptchaService.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace captchademov3.Models
+{
+    public class GoogleCaptchaService
+    {
+        public async Task<bool> verifycaptcha(string _Token)
+        {
+            
+            using(var client = new HttpClient())
+            {
+              var values=new Dictionary<string, string>
+                {
+                  { "response" , _Token },
+                  { "secret" , "6LezVY0dAAAAAHXWLv68LypHJd6NYloRbcgNBLeQ" }
+                };
+                var content = new FormUrlEncodedContent(values);
+                var verify = await client.PostAsync("https://www.google.com/recaptcha/api/siteverify", content);
+                var captcharesponsejson=await verify.Content.ReadAsStringAsync();
+                var captcharesult=JsonConvert.DeserializeObject<GoogleCaptchaRespo>(captcharesponsejson);
+                return captcharesult.success  && captcharesult.score > 0.5;
+            }
+        }
+    }
+
+    public class GoogleCaptchaData
+    {
+        public string response { get; set; }
+        public string secret { get; set; }
+    }
+    public class GoogleCaptchaRespo
+    {
+        public bool success { get; set; }
+        public double score { get; set; }
+        public string action { get; set; }
+        public DateTime challenge_ts { get; set; }
+        public string hostname { get; set; }
+    }
+}
